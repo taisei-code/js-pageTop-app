f166ab1bda59b8581fbe8f3be29722d48410a0d4
Taisei <taisei96.web@gmail.com>
2023-05-08 08:22:07 +0900

    README記入

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 'use strict';
-
+// body要素のスクロール量を取得、0が返ってくる
 const windowTop = document.body.scrollTop;
-
+// #page-topを取得し、変数に代入
 const pageTop   = document.getElementById('page-top');
-
+// pageTopをクリックした時の動作
 pageTop.addEventListener('click', (e) => {
   e.preventDefault();
 
