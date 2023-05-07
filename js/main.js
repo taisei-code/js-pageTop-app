23bbc11dd460058a3c002937a03580e64a427202
Taisei <taisei96.web@gmail.com>
2023-05-08 08:14:33 +0900

    トップへ戻る実装

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const windowTop = document.body.scrollTop;
+
+const pageTop   = document.getElementById('page-top');
+
+pageTop.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  window.scrollTo({
+    top: windowTop,
+    behavior: 'smooth'
+  });
+});
